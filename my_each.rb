21fd8241133e39d2325e6f037dd32282b56eefac
Samantha brake <sammy.brake@gmail.com>
2018-08-29 19:02:12 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,9 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  counter = 0 
+  while counter < array.length 
+  yield array[counter]
+  
+  counter = counter.next 
+end 
+array 
 end
